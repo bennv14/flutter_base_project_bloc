17b107199e82b5538f2e94171a7f01968f8c9206
bennv <nguyenvanben02001@gmail.com>
2024-11-30 17:18:00 +0700

    feat: add base usecase

--- lib/base/domain/usecase/use_case.dart
+++ lib/base/domain/usecase/use_case.dart
@@ -1,1 +1,17 @@
-class UseCase {}
+abstract class BaseUseCase<Type, Input> {
+  Future<Type> execute(Input input);
+}
+
+abstract class UseCaseIO<Type, Input> extends BaseUseCase<Type, Input> {
+  Future<Type> build(Input input);
+
+  @override
+  Future<Type> execute(Input input);
+}
+
+abstract class UseCase<Type> extends BaseUseCase<Type, Null> {
+  Future<Type> build();
+
+  @override
+  Future<Type> execute([Null input]);
+}
